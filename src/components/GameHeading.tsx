ca352f59e897794a2086a401d0f018e115b70096
moise10r <moiserushanika2006@gmail.com>
2023-04-26 15:38:40 +0200

    Implement a custom hook to query games from store.

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -4,7 +4,8 @@ import usePlatform from '../hooks/usePlatform';
 import useGameQueryStore from '../store';
 
 const GameHeading = () => {
- 
+  const genreId = useGameQueryStore(s => s.gameQuery.genreId);
+  const genre = useGenre(genreId);
 
   const platformId = useGameQueryStore(s => s.gameQuery.platformId);
   const platform = usePlatform(platformId);
