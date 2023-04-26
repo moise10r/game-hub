14355e006e40bf2383432d8208a93a257d4b3517
moise10r <moiserushanika2006@gmail.com>
2023-04-26 16:18:22 +0200

    Make the Entities available and make api calls for games.

---
+++ src/services/api-client.ts
@@ -0,0 +1,36 @@
+import axios, { AxiosRequestConfig } from 'axios';
+
+export interface FetchResponse<T> {
+  count: number;
+  next: string | null;
+  results: T[];
+}
+
+const axiosInstance = axios.create({
+  baseURL: 'https://api.rawg.io/api',
+  params: {
+    key: 'e53bba19d7914970889f528d70c8e06d',
+  },
+});
+
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+
+  get = (id: number | string) => {
+    return axiosInstance
+      .get<T>(this.endpoint + '/' + id)
+      .then((res) => res.data);
+  };
+}
+
+export default APIClient;
