2c844fd93be864dc1455fee118ef5527208ecdeb
moise10r <moiserushanika2006@gmail.com>
2023-04-26 15:38:02 +0200

    Refactor the heading view.

---
+++ src/components/GameHeading.tsx
@@ -0,0 +1,23 @@
+import { Heading } from '@chakra-ui/react';
+import useGenre from '../hooks/useGenre';
+import usePlatform from '../hooks/usePlatform';
+import useGameQueryStore from '../store';
+
+const GameHeading = () => {
+ 
+
+  const platformId = useGameQueryStore(s => s.gameQuery.platformId);
+  const platform = usePlatform(platformId);
+
+  const heading = `${platform?.name || ''} ${
+    genre?.name || ''
+  } Games`;
+
+  return (
+    <Heading as="h1" marginY={5} fontSize="5xl">
+      {heading}
+    </Heading>
+  );
+};
+
+export default GameHeading;
